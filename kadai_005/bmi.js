7a9fb59984a1df72bdfc553a42aa9556de45f456
Garamumasara <153249333+Garamumasara@users.noreply.github.com>
2024-03-21 16:37:20 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+//変数の宣言・値の代入（体重）
+let bodyWeight = 68;
+
+//コンソールへの出力
+console.log(bodyWeight);
+
+//変数の宣言・値の代入（身長）
+let bodyHeight = 1.7;
+
+//コンソールへの出力
+console.log(bodyHeight);
+
+//変数の宣言・値の代入（計算式）
+let num = bodyWeight / (bodyHeight * bodyHeight);
+
+//コンソールへの出力
+console.log(num);

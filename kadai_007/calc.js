73774031d39db4f8e70bf52b3bad8498bfbca18a
Garamumasara <153249333+Garamumasara@users.noreply.github.com>
2024-03-23 10:45:29 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+//変数numに0~50までのランダムな整数を代入
+let num = Math.floor(Math.random() * 51);
+
+//変数numの値を出力する
+console.log(num);
+
+//ifの条件
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log(`3と5の倍数です`);
+} else if (num % 3 === 0) {
+  console.log(`3の倍数です`);
+} else if (num % 5 === 0) {
+  console.log(`5の倍数です`);
+} else {
+  console.log (num);
+}
